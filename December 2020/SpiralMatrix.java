58ef4328ab831448aeda1f23dc6080ef6c02721a
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2020-12-07 16:46:17 +0530

    day 7

---
+++ December 2020/SpiralMatrix.java
@@ -0,0 +1,8 @@
+/*
+Given a positive integer n, generate an n x n matrix filled with elements from 1 to n2 in spiral order.
+Input: n = 3
+Output: [[1,2,3],[8,9,4],[7,6,5]]
+Input: n = 1
+Output: [[1]]
+*/
+
