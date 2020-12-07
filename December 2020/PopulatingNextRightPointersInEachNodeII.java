6e8adb1ca91edeb27bdc86d7700a5973b4225ebb
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2020-12-07 16:35:37 +0530

    Update PopulatingNextRightPointersInEachNodeII.java

--- December 2020/PopulatingNextRightPointersInEachNodeII.java
+++ December 2020/PopulatingNextRightPointersInEachNodeII.java
@@ -17,3 +17,55 @@ Explanation: Given the above binary tree (Figure A), your function should popula
 is in level order as connected by the next pointers, with '#' signifying the end of each level.
 */
 
+// Using queue
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null) 
+            return root;
+        
+        Queue<Node> queue = new LinkedList();
+        queue.offer(root);
+        
+        while(queue.size()>0) {
+            int size = queue.size();
+            Node previous = null;
+            while(size-->0) {
+                Node node = queue.poll();
+                node.next = previous ;
+                previous = node;
+                if(node.right!=null)
+                    queue.offer(node.right);
+                if(node.left!=null)
+                    queue.offer(node.left);
+            }
+        }   
+        
+        return root;
+    }
+}
+
+// using O(1) space
