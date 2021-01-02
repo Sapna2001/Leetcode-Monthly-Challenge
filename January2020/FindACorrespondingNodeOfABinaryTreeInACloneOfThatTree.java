b3aade32a64cbb3b30055057fb6d78792c3c3b10
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-01-02 14:34:09 +0530

    day2

--- January2020/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree.java
+++ January2020/FindACorrespondingNodeOfABinaryTreeInACloneOfThatTree.java
@@ -6,6 +6,8 @@ Return a reference to the same node in the cloned tree.
 Note that you are not allowed to change any of the two trees or the target node and the answer must be a reference to a node in the cloned tree.
 
 Link - https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3590/
+TC - O(n)
+SC - O(n)
 
 */
 
