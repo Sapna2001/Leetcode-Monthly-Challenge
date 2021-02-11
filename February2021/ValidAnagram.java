9ec2d70b1ceaeaf4adf8168be77c797eb2f78136
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-02-11 14:38:15 +0530

    day11

---
+++ February2021/ValidAnagram.java
@@ -0,0 +1,15 @@
+/*
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Link - https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/585/week-2-february-8th-february-14th/3636/
+*/
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char sArray[] = s.toCharArray();
+        Arrays.sort(sArray);
+        char tArray[] = t.toCharArray();
+        Arrays.sort(tArray);
+        return Arrays.equals(sArray,tArray);
+    }
+}
