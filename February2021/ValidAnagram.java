81cb9abb20931b3f2a3e9766b9c98ff813c409a4
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-02-11 14:41:19 +0530

    day11

--- February2021/ValidAnagram.java
+++ February2021/ValidAnagram.java
@@ -2,6 +2,8 @@
 Given two strings s and t , write a function to determine if t is an anagram of s.
 
 Link - https://leetcode.com/explore/challenge/card/february-leetcoding-challenge-2021/585/week-2-february-8th-february-14th/3636/
+// hashtable can also be used
+// use hashmap for the unicode solution
 */
 
 class Solution {
