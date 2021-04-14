95d7ea6f47dd7d94a7c87777e4a7e98f6658f5c6
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-04-14 14:41:24 +0530

    day14

---
+++ April2021/PartitionList.java
@@ -0,0 +1,30 @@
+/*
+Given the head of a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Link: https://leetcode.com/explore/challenge/card/april-leetcoding-challenge-2021/594/week-2-april-8th-april-14th/3707/
+*/
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode head1 = new ListNode(-1);
+        ListNode head2 = new ListNode(-1);
+        ListNode small = head1;
+        ListNode large = head2;  
+        ListNode current = head;
+        while(current != null) {
+            if(x > current.val) {
+                small.next = current;
+                small = current;
+            } else {
+               large.next = current;
+               large = current; 
+            }
+            current = current.next;
+        }
+        large.next = null;
+        small.next = head2.next;
+        return head1.next;
+    }
+}
