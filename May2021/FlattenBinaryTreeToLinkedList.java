a90312eeff56ba2a0b0d8c9372cfec1ce5bbddeb
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-05-15 23:56:08 +0530

    day14

--- May2021/FlattenBinaryTreeToLinkedList.java
+++ May2021/FlattenBinaryTreeToLinkedList.java
@@ -6,3 +6,32 @@ Given the root of a binary tree, flatten the tree into a "linked list":
 
 Link: https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3742/
 */
+
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null || (root.left == null && root.right == null))
+            return;
+        preOrderTraversal(root);
+    }
+    
+    TreeNode preOrderTraversal(TreeNode root) {
+        if(root == null)
+            return null;
+        
+        TreeNode leftChild = preOrderTraversal(root.left);
+        TreeNode rightChild = preOrderTraversal(root.right);
+        
+        TreeNode extremeRightOfLeftChild = leftChild;
+        while(extremeRightOfLeftChild != null && extremeRightOfLeftChild.right != null)
+            extremeRightOfLeftChild = extremeRightOfLeftChild.right;
+        
+        if(extremeRightOfLeftChild != null) {
+            extremeRightOfLeftChild.right = rightChild;
+            root.right = leftChild;
+        }
+        
+        root.left = null;
+        return root;
+        
+    } 
+}
