62cb600cae8ee47d4683a110e40fce9027005238
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-05-15 21:08:35 +0530

    day14

---
+++ May2021/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,8 @@
+/*
+Given the root of a binary tree, flatten the tree into a "linked list":
+
+- The "linked list" should use the same TreeNode class where the right child pointer points to the next node in the list and the left child pointer is always null.
+- The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+
+Link: https://leetcode.com/explore/challenge/card/may-leetcoding-challenge-2021/599/week-2-may-8th-may-14th/3742/
+*/
