61775c8e0f210f833ea3e8ff6029a2f263d5436c
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-06-06 00:31:11 +0530

    day5

--- June2021/MaximumPerformanceOfATeam.java
+++ June2021/MaximumPerformanceOfATeam.java
@@ -10,3 +10,38 @@ Return the maximum performance of this team. Since the answer can be a huge numb
 
 Link: https://leetcode.com/explore/featured/card/june-leetcoding-challenge-2021/603/week-1-june-1st-june-7th/3768/
 */
+
+class Solution {
+    private class Engineer {
+        private int speed;
+        private int efficiency;
+        
+        public Engineer(int speed, int efficiency) {
+            this.speed = speed;
+            this.efficiency = efficiency;
+        }
+    }
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        List<Engineer> engineers = new ArrayList<>();
+        for(int i = 0 ; i < n ; i++)
+            engineers.add(new Engineer(speed[i], efficiency[i]));
+        engineers.sort((a,b) -> b.efficiency - a.efficiency);
+        PriorityQueue<Engineer> currentTeam = new PriorityQueue<>((a,b) -> a.speed - b.speed);
+        
+        long speedSum = 0;
+        long maxPerformance = 0;
+        
+        for(Engineer engineer : engineers) {
+            if(currentTeam.size() == k) {
+                Engineer minSpeed = currentTeam.poll();
+                speedSum -= minSpeed.speed;
+            }
+            currentTeam.add(engineer);
+            speedSum += engineer.speed;
+            
+            long currentPerformance = speedSum * (long) engineer.efficiency;
+            maxPerformance = Math.max(maxPerformance, currentPerformance);
+        }
+        return (int)(maxPerformance % 1000000007);
+    }
+}
