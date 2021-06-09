77296bfe300c878cb1e462f6c490bc00e431041f
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-06-09 21:29:53 +0530

    day9

--- June2021/JumpGameVI.java
+++ June2021/JumpGameVI.java
@@ -9,3 +9,31 @@ You want to reach the last index of the array (index n - 1). Your score is the s
 Return the maximum score you can get.
 */
 
+class Solution {
+    public int maxResult(int[] nums, int k) {
+        if(nums.length == 0)
+            return 0;
+        
+        Queue<int[]> priorityQueue = new PriorityQueue<>((a,b) -> (b[0] - a[0]));
+        
+        priorityQueue.offer(new int[]{nums[0], 0});
+        
+        int max = nums[0];
+        
+        for(int i = 1; i < nums.length ; i++) {
+            while(i - priorityQueue.peek()[1] > k) {
+                priorityQueue.poll();
+            }
+            
+            int[] current = priorityQueue.peek();
+            max = current[0] + nums[i];
+            priorityQueue.offer(new int[]{max, i});
+        }
+        return max;
+    }
+}
+
+/* 
+TC: O(nlogk)
+SC: O(k)
+*/
