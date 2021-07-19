2cb228cc7561cc66a01d2a1f55eefc7149dfef4c
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-07-19 14:57:04 +0530

    day19

--- July2021/LowestCommonAncestorofaBinarySearchTree.java
+++ July2021/LowestCommonAncestorofaBinarySearchTree.java
@@ -16,12 +16,11 @@ Link: https://leetcode.com/explore/challenge/card/july-leetcoding-challenge-2021
  *     TreeNode(int x) { val = x; }
  * }
  */
-
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root.val > p.val && root.val > q.val) {
             return lowestCommonAncestor(root.left,p,q); 
-        } else if(root.val < p.val && root.val > q.val) {
+        } else if(root.val < p.val && root.val < q.val) {
             return lowestCommonAncestor(root.right,p,q); 
         } else {
             return root;
