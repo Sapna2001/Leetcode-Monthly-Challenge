606e5c2c63027623d1b3f40ac96e3632d93e6cf5
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-08-24 16:03:42 +0530

    day24

--- August2021/ComplexNumberMultiplication.java
+++ August2021/ComplexNumberMultiplication.java
@@ -1,4 +1,11 @@
 /*
+A complex number can be represented as a string on the form "real+imaginaryi" where:
+
+real is the real part and is an integer in the range [-100, 100].
+imaginary is the imaginary part and is an integer in the range [-100, 100].
+i2 == -1.
+Given two complex numbers num1 and num2 as strings, return a string of the complex number that represents their multiplications.
+
 Link: https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/616/week-4-august-22nd-august-28th/3917/
 
 Time complexity : O(1)
