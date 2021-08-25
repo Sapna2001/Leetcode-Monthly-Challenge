be03cb57f529e2b49c9cf5ae236a1525c6932c33
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-08-25 14:59:59 +0530

    Create SumOfSquareNumbers.java

---
+++ August2021/SumOfSquareNumbers.java
@@ -0,0 +1,19 @@
+/*
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+Link: https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/616/week-4-august-22nd-august-28th/3918/
+*/
+
+public class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = Math.sqrt(c - a * a);
+            if (b == (int) b)
+                return true;
+        }
+        return false;
+    }
+}
+
+// TC: O(sqrt(c) logc)
+// SC: O(1)
