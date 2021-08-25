b56fee71af33be5ba9c286e5461f54b0fffaccb7
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-08-25 15:00:33 +0530

    day25

--- August2021/SumOfSquareNumbers.java
+++ August2021/SumOfSquareNumbers.java
@@ -2,6 +2,9 @@
 Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
 
 Link: https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/616/week-4-august-22nd-august-28th/3918/
+
+TC: O(sqrt(c) logc)
+SC: O(1)
 */
 
 public class Solution {
@@ -15,5 +18,4 @@ public class Solution {
     }
 }
 
-// TC: O(sqrt(c) logc)
-// SC: O(1)
+
