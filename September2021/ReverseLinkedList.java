944313cbd887d029cf6fe1638e40661a85590351
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-09-07 20:01:22 +0530

    Create ReverseLinkedList.java

---
+++ September2021/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+/*
+https://leetcode.com/explore/challenge/card/september-leetcoding-challenge-2021/636/week-1-september-1st-september-7th/3966/
+*/
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode current = head, previous = null, next = null;
+        
+        while(current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        
+        return previous;
+    }
+}
