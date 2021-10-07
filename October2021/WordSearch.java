212fe3df89ef4c2f4dabf74a2f95ca63ccea74f4
Hari Sapna Nair <56690856+Sapna2001@users.noreply.github.com>
2021-10-07 18:05:35 +0530

    day7

--- October2021/WordSearch.java
+++ October2021/WordSearch.java
@@ -13,7 +13,7 @@ class Solution {
         
         for(int i = 0 ; i < board.length ; i++) {
             for(int j = 0 ; j < board[0].length ; j++) {
-                if(charArray[i] == board[i][j]) {
+                if(charArray[0] == board[i][j]) {
                     boolean[][] visited = new boolean[board.length][board[0].length];
                     if(dfs(board, charArray, i, j, visited, 0)) {
                         return true;
@@ -38,6 +38,8 @@ class Solution {
             return false;
         }
         
+        visited[i][j] = true;
+        
         if((dfs(board, word, i + 1, j, visited, index + 1)) || 
            (dfs(board, word, i, j + 1, visited, index + 1)) ||
            (dfs(board, word, i - 1, j, visited, index + 1)) ||
@@ -49,4 +51,3 @@ class Solution {
         return false;
     }
 }
-
